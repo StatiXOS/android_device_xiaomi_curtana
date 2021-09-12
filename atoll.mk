5304b126b22611b9f08afde2983132f8bf82d5d7
BladeRunner-A2C <prisoner56898@protonmail.com>
2021-09-12 10:54:26 +0200

    miatoll: Build QTI camera HAL
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- atoll.mk
+++ atoll.mk
@@ -90,7 +90,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
