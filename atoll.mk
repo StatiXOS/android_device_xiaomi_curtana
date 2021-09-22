3db99838d33e3b82b2b77c0f17af397ed9181293
Alexander Winkowski <dereference23@outlook.com>
2021-09-22 17:31:50 +0200

    miatoll: Update sensors service to 2.1
    Change-Id: Ie417407c36929cf34ecbdd28feb1cab996fe1f30

--- atoll.mk
+++ atoll.mk
@@ -340,7 +340,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Servicetracker
