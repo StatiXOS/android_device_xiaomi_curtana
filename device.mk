420273d8521ea02cbe84b96524f3e4d20d47e6bb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-23 16:49:09 +0200

    curtana: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352

--- device.mk
+++ device.mk
@@ -209,7 +209,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_curtana
+    android.hardware.light-service.xiaomi
 
 # Media
 PRODUCT_PACKAGES += \
