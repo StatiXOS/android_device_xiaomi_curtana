3baf6e5159a2a17900e5d463e42ead086b0a75ef
BladeRunner-A2C <prisoner56898@protonmail.com>
2021-10-11 02:14:24 +0600

    miatoll: Compile with proton clang
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,8 +96,9 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := cust_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CONFIG := vendor/miatoll-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/msm-4.14
 
 BOARD_KERNEL_CMDLINE += \
     androidboot.console=ttyMSM0 \
