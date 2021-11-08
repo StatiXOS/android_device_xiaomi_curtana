560c5b8050ebbcc60287e845234caa3dc52945e1
Devin Moore <devinmoore@google.com>
2021-11-08 17:52:47 +0000

    miatoll: Switch to new AIDL IR service
    Test: atest VtsHalIrTargetTest hal_implementation_test
    Bug: 205000342
    Change-Id: I70c54aa8e8962939cc8f4b42fbbe828e9dad7f0d
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -200,8 +200,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Kernel
 PRODUCT_COPY_FILES += \
