184f75323dec15ebdc61645a3ff70b0dd2f00b9c
Bowgo Tsai <bowgotsai@google.com>
2021-12-09 18:59:20 +0800

    curtana: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger
    
    Change-Id: I0cf78bfc80c24bb217bb88831faa5a29ba04d194

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@ endif
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
