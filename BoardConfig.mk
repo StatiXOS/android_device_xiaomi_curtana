eee4903499b156f1768476774b91480e48d1bf49
LeddaZ <leonardoledda@gmail.com>
2021-12-31 02:08:52 +0100

    miatoll: build kernel with AOSP clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,6 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CONFIG := vendor/miatoll-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
 
