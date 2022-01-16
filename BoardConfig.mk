94d13b20c62fda4fc1ed2483fa4a64d7671325a4
LeddaZ <leonardoledda@gmail.com>
2022-01-16 14:41:57 +0100

    miatoll: drop SELINUX_IGNORE_NEVERALLOWS
    * Source side neverallows have been fixed long ago

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -210,8 +210,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy/vendor
 
-SELINUX_IGNORE_NEVERALLOWS := true
-
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-09-01
 
