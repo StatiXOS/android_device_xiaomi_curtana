accc4c9d8158c35a16698b82aa61d43cee40858f
Etienne Ruffieux <eruffieux@google.com>
2022-01-20 21:27:18 +0000

    miatoll: Added new vendors for Bluetooth config migration
    Tag: #feature
    Test: manual
    Bug: 211570675
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Id74ec5e1b30f21c534c35b212a2fb297e9d591fb

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    bluetooth.device.class_of_device=90,2,12
+    bluetooth.device.class_of_device=90,2,12 \
+    bluetooth.profile.asha.central=false
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.bluetooth.modem_nv_support=true
