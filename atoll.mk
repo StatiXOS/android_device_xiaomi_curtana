805c7191749f33486c4ed6667cf544a8a2e5b237
TheScarastic <warabhishek@gmail.com>
2022-02-08 02:55:40 +0600

    miatoll: Create a button jack keylayout for headphones
    Xiaomi sends a custom keycode while trying to adjust volume through
    in-headphone volume buttons so add a custom keylayout to handle such
    usecase.
    
    Squashed: Support play/pause in headphone jack keylayout
    
    Change-Id: I86c6a84051bd0c23b3b25eb02f070fbdc8a1376c
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -246,6 +246,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
