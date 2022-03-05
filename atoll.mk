40ebb1f45346badcdda5ea38e3a00e933b4175da
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-03-06 00:43:01 +0600

    miatoll: biometrics: Rebrand to miatoll
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- atoll.mk
+++ atoll.mk
@@ -209,7 +209,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_atoll
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_miatoll
 
 # Fwk detect
 PRODUCT_SYSTEM_PROPERTIES += \
