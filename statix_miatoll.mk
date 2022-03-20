3e9b16de79365774478a73dff8a12752b73b5553
LeddaZ <leonardoledda@gmail.com>
2022-03-20 18:09:41 +0100

    miatoll: drop Pixel Launcher

--- statix_miatoll.mk
+++ statix_miatoll.mk
@@ -25,8 +25,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Pixel Launcher
-ifneq ($(TARGET_DOES_NOT_USE_GAPPS),true)
-INCLUDE_PIXEL_LAUNCHER := true
-endif
