a61af25858c9239c6d195dca6b2106e2721f0935
Sal Savage <salsavage@google.com>
2022-04-25 16:42:15 -0700

    miatoll: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I07e83ca352ff969be0e53a0f63b6c940b6c34dfb

--- device.mk
+++ device.mk
@@ -89,6 +89,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    bluetooth.device.class_of_device=90,2,12
+
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.bluetooth.modem_nv_support=true
 
