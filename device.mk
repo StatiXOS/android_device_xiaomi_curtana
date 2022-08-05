7653953690663554cc7710336873fa9e5c6a8220
Alexander Winkowski <dereference23@outlook.com>
2022-08-05 11:48:32 +0000

    curtana: Fix AAPT config
    According to AOSP docs [1] apps should use xxhdpi resources
    for 440 dpi density.
    
    [1] https://developer.android.com/reference/android/util/DisplayMetrics#DENSITY_440
    
    Change-Id: Ib591e11a93b9814fabf772136096bde69e202e77

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Display / Graphics
 PRODUCT_PACKAGES += \
