22dd174b8fcb4b02a9c55d5b28c8d3f39bf5825f
LeddaZ <leonardoledda@gmail.com>
2022-08-23 13:29:41 +0200

    Revert "miatoll: Switch to QTI power AIDL"
    This reverts commit dfa9797cd0322bf7e5aae6b76e48ed72e5bf0809.

--- device.mk
+++ device.mk
@@ -425,7 +425,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.xiaomi-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Pre-Rendering
 PRODUCT_SYSTEM_PROPERTIES += \
