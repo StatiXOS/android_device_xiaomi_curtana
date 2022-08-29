b8d9ddd2a86257e1d8b0618444edbc27ba87357d
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    curtana: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,7 +188,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Selinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy/vendor
