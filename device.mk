e95bef2b96945c81b4cde639b37f47a5e3b7dec3
LeddaZ <leonardoledda@gmail.com>
2022-10-18 18:28:52 +0200

    curtana: Drop QTI WFD

--- device.mk
+++ device.mk
@@ -462,14 +462,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit proprietary blobs
 $(call inherit-product, vendor/xiaomi/curtana/curtana-vendor.mk)
