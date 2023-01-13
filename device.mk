8a81c6b082e0e6f4a08d1ea384f166cdd813d745
LeddaZ <leonardoledda@gmail.com>
2023-01-13 16:07:58 +0100

    curtana: Update hardware/xiaomi namespace

--- device.mk
+++ device.mk
@@ -408,7 +408,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    vendor/hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
