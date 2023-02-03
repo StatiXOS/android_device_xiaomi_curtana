c5f2ce3711d48f67d2a7624cedded6f9fe4da947
subhajeetmuhuri <subhajeet.muhuri@gmail.com>
2023-02-03 22:39:39 +0100

    curtana: sepolicy: Label more wakeup nodes
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,8 +180,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Selinux
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2022-10-01
