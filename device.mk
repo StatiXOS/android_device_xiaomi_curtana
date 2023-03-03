fa42316b8ac904a8a04e766df132ec0d881db472
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    curtana: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
     android.hardware.keymaster@4.1.vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
