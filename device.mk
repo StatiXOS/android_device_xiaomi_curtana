de89642204eeb4f99fdb3f09b703d5883df709fc
Leonardo Ledda <leonardoledda@gmail.com>
2023-03-05 20:43:48 +0100

    curtana: Copy some GPS configs to device tree
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -159,6 +159,9 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
