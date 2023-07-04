04752c11fab64bd50ef685b5fbe5e60f35512dbc
Leonardo Ledda <leonardoledda@gmail.com>
2023-07-04 17:19:26 +0200

    curtana: Update firmware message in releasetools
    * Make it clearer
    * Also fix indentation
    
    Change-Id: Ic449d46a8f3b66acf2fe455d64e8647aba93d941
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -23,12 +23,12 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def AddImageRadio(info, basename, dest):
-    name = basename
-    if ("RADIO/" + basename) in info.input_zip.namelist():
-        data = info.input_zip.read("RADIO/" + basename)
-        common.ZipWriteStr(info.output_zip, name, data)
-        info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-        info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+  name = basename
+  if ("RADIO/" + basename) in info.input_zip.namelist():
+    data = info.input_zip.read("RADIO/" + basename)
+    common.ZipWriteStr(info.output_zip, name, data)
+    info.script.Print("Flashing {} image...".format(dest.split('/')[-1]))
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching dtbo and vbmeta images...")
@@ -36,22 +36,23 @@ def OTA_InstallEnd(info):
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
   
-  info.script.Print("Patching firmware images...")
-  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl");
-  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop");
-  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");
-  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib");
-  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64");
-  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg");
-  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp");
-  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp");
-  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv");
-  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster");
-  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");
-  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw");
-  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec");
-  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz");
-  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp");
-  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config");
-  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl");
+  info.script.Print("Flashing firmware...")
+  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl")
+  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop")
+  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
+  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
+  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
+  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
+  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
+  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp")
+  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv")
+  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
+  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
+  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw")
+  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec")
+  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
+  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp")
+  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config")
+  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl")
+  info.script.Print("Firmware flashed succesfully.")
   return
