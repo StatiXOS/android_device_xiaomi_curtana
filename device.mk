403b6ffa7911611a51666798d89dbed510d4a72f
Leonardo Ledda <leonardoledda@gmail.com>
2023-07-24 00:16:16 +0200

    curtana: Drop prebuilt GCam
    * Switching to Aperture/Flash
    
    Change-Id: I6808926ff52a78652643f95cd8a895cbd5f2ec0f
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -128,9 +128,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
-# Google Camera
-$(call inherit-product, vendor/xiaomi/curtana-gcam/curtana-gcam-vendor.mk)
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
