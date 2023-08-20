4aebcb47f87d4a5102f436925ef8644a6b38564c
Alexander Winkowski <dereference23@outlook.com>
2023-08-20 12:15:33 +0000

    miatoll: Switch to SNXXX NFC service
    Despite having the _snxxx suffix, it is essentially an
    updated NXP NFC HAL that also works with PN5xx chips.
    
    Change-Id: I70911582c84f5ba1eb67f767887c110170655860

--- device.mk
+++ device.mk
@@ -239,7 +239,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service
+    android.hardware.nfc_snxxx@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/nfc/,$(TARGET_COPY_OUT_VENDOR)/etc)
