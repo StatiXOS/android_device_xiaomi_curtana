e83f98c5c0b3dd57740a0175f1175dd72fd1f810
Leonardo Ledda <leonardoledda@gmail.com>
2023-08-28 18:52:40 +0200

    miatoll: Don't include firmware
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -22,37 +22,9 @@ def AddImage(info, basename, dest):
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def AddImageRadio(info, basename, dest):
-  if ("RADIO/" + basename) in info.input_zip.namelist():
-    data = info.input_zip.read("RADIO/" + basename)
-    path = "firmware-update/" + basename
-    common.ZipWriteStr(info.output_zip, path, data)
-    info.script.Print("Flashing {} image...".format(dest.split('/')[-1]))
-    info.script.AppendExtra('package_extract_file("%s", "%s");' % (path, dest))
-
 def OTA_InstallEnd(info):
   info.script.Print("Patching dtbo and vbmeta images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
-  
-  info.script.Print("Flashing firmware...")
-  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl")
-  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop")
-  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
-  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
-  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
-  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
-  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
-  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp")
-  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv")
-  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
-  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
-  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw")
-  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec")
-  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
-  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp")
-  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config")
-  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl")
-  info.script.Print("Firmware flashed succesfully.")
   return
