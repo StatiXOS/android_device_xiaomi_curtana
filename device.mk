34c1ca700e5e61833e62821cf989a00e64923576
Giovanni Ricca <giovanniricca@proton.me>
2023-12-06 09:50:13 +0100

    miatoll: Build goodix lib from source
    Change-Id: Ibaf1b9489b509318032c720c9c83e315cfe6ab3e

--- device.mk
+++ device.mk
@@ -133,6 +133,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+PRODUCT_PACKAGES += \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
