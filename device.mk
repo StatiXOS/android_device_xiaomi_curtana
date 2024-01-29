8d1f2540c7ec5f74c4e63a21db0686293fcb38b7
Leonardo Ledda <leonardoledda@gmail.com>
2024-01-29 22:48:10 +0100

    Revert "miatoll: Add more prebuilt camera provider libs"
    This reverts commit 04619e5f24c849b6918e6c6cff95f1b2a1f26e64.
    
    Change-Id: Ib2ffc4ee1cf294b255396342cb133741f07114f3
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -65,12 +65,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
     libmegface \
     libMegviiFacepp-0.5.2 \
     libstdc++_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
