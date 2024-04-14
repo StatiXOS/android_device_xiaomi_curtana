62fdca34f1501af6feb73ff93e845d0e675d6ec7
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-14 20:02:25 +0200

    miatoll: Inherit common Xiaomi FCM
    Change-Id: I252143f93f76f28da8f4427e4e42881a23316b1a
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
