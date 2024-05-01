459865706cc209da24b98646d48e9a82966172c0
Leonardo Ledda <leonardoledda@gmail.com>
2024-05-01 19:33:42 +0200

    miatoll: Inherit fingerprint split makefile
    Change-Id: Iedb44c46f4847ca84ec2aec22e43c19b3d34de79
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -131,8 +131,7 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+$(call inherit-product, vendor/hardware/xiaomi/aidl/power-libperfmgr/power_product.mk)
 
 PRODUCT_PACKAGES += \
     com.fingerprints.extension@1.0.vendor \
@@ -351,7 +350,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
