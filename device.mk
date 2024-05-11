4dcfea9ecfef556b50841afd852cbe084dabc087
Alexander Winkowski <dereference23@outlook.com>
2024-05-11 02:12:04 +0200

    miatoll: Drop dependencies of OSS display components
    They're built automatically
    
    Change-Id: I7a113dd1fcede08395f292a630405ac15803d22d

--- device.mk
+++ device.mk
@@ -98,28 +98,17 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.5 \
-    vendor.display.config@1.11.vendor \
-    vendor.display.config@2.0 \
-    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 PRODUCT_PACKAGES += \
     disable_configstore \
     gralloc.atoll \
     hwcomposer.atoll \
-    libdisplayconfig.qti \
     libdisplayconfig.system.qti \
-    libqdMetaData \
     libqdMetaData.system \
-    libsdmcore \
-    libsdmutils \
     libtinyxml \
-    libvulkan \
     memtrack.atoll
 
 # DRM
