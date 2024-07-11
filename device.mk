0b3f4163d67ae1247882d2c06a0a78301e3bb54b
Leonardo Ledda <leonardoledda@gmail.com>
2024-07-11 20:43:11 +0200

    miatoll: Pull msm_irqbalance.conf from vendor
    Change-Id: Ie243f123c42d539f5deec8752d5b6806d5ea35f9
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -327,9 +327,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
