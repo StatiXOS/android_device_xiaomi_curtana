c9c4f6f5cc17f9f0ef9661cebcd2108f2a7a7e7a
Leonardo Ledda <leonardoledda@gmail.com>
2024-09-08 02:03:57 +0200

    miatoll: Sync extract-files scripts with latest templates
    Change-Id: I66b4cf4855c9ad949da9497b2eb1c02fea6916a5
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020, 2022 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -16,6 +15,10 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -32,19 +35,20 @@ SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -57,35 +61,54 @@ function blob_fixup() {
     case "${1}" in
         # Suppress CamX debug
         vendor/etc/camera/camxoverridesettings.txt)
+            [ "$2" = "" ] && return 0
             sed -i "s/0x10080/0/g" "${2}"
             sed -i "s/0x1F/0x0/g" "${2}"
             ;;
         # Use VNDK 32 libhidlbase
         vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            [ "$2" = "" ] && return 0
             sed -i "s|libhidlbase.so|v32hidlbase.so|g" "${2}"
             ;;
         # Load keymaster 4.1
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
+            [ "$2" = "" ] && return 0
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;
         # Fix camera in 3rd party apps
         vendor/lib64/camera/components/com.qti.node.watermark.so)
+            [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "$2" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         # NOP out report_input_event()
         vendor/lib64/hw/fingerprint.fpc.default.so)
+            [ "$2" = "" ] && return 0
             "${SIGSCAN}" -p "30 00 00 90 11 3a 42 f9" -P "30 00 00 90 1f 20 03 d5" -f "${2}"
             ;;
         # Patch camera provider to load the new symbols
         vendor/lib64/android.hardware.camera.provider@2.4-legacy.so)
+            [ "$2" = "" ] && return 0
             grep -q "libcamera_provider_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_provider_shim.so" "${2}"
             ;;
+            *)
+                return 1
+                ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+if [ -z "${SECTION}" ]; then
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+fi
 
 "${MY_DIR}/setup-makefiles.sh"
